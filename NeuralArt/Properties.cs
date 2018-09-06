c1531e7c9a918ee92a897a2930928d0ac1164a48
Глеб Брыкин <glebbrykin@yandex.ru>
2018-09-06 19:58:13 +0300

    Add files via upload

---
+++ NeuralArt/Properties.cs
@@ -0,0 +1,18 @@
+﻿//*************************************************************************************************
+//* Код является разработкой PABCSoft(C. Брыкин Глеб Сергеевич, 2018). Коммерческое использование
+//* запрещено. При любом ином использовании указать ссылку на текущий продукт.
+//*************************************************************************************************
+
+using System.Reflection;
+
+  //-> Подпись сборки.
+
+  [assembly: AssemblyTitle("NeuralArt")]
+  [assembly: AssemblyDescription("Image style transfer")]
+  [assembly: AssemblyConfiguration("")]
+  [assembly: AssemblyVersion("0.26.08.2018")]
+  [assembly: AssemblyCompany("PABCSoft")]
+  [assembly: AssemblyProduct("NeuralArt")]
+  [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
+  [assembly: AssemblyTrademark("ImageInstruments")]
+  [assembly: AssemblyCulture("")]
